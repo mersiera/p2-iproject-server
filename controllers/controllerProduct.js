3050f0dec3fc5d1c4c35059437fcd6d5e28bfa06
mersiera <mersiera18@gmail.com>
2022-04-22 09:50:45 +0700

    revisi

--- controllers/controllerProduct.js
+++ controllers/controllerProduct.js
@@ -65,22 +65,22 @@ class productController {
       await Product.update({ UserId: idUser }, { where: { id: productId } });
       await History.create({ seller: findSeller.email, buyer: email, ProductId: productId });
 
-      // switch (message) {
-      //   case "success":
-      //     message = `Dear ${email}, you have success buy ${findProduct.name}!`;
-      //     statusBuy = "Success";
-      //     break;
-      //   case "pending":
-      //     message = `Dear ${email}, you have success buy ${findProduct.name}!`;
-      //     statusBuy = "Pending";
-      //     break;
-      //   case "failed":
-      //     message = `Dear ${email}, you have success buy ${findProduct.name}!`;
-      //     statusBuy = "Failed";
-      //     break;
-      //   default:
-      //     break;
-      // }
+      switch (message) {
+        case "success":
+          message = `Dear ${email}, you have success buy ${findProduct.name}!`;
+          statusBuy = "Success";
+          break;
+        case "pending":
+          message = `Dear ${email}, you have success buy ${findProduct.name}!`;
+          statusBuy = "Pending";
+          break;
+        case "failed":
+          message = `Dear ${email}, you have success buy ${findProduct.name}!`;
+          statusBuy = "Failed";
+          break;
+        default:
+          break;
+      }
 
       // let testAccount = await nodemailer.createTestAccount();
 
@@ -91,8 +91,7 @@ class productController {
       //     pass: process.env.EMAIL_PASSWORD, // generated ethereal password
       //   },
       // });
-      // // findSeller.emailz
-      // //
+
       // let info = await transporter.sendMail({
       //   from: findSeller.email, // sender address
       //   to: email, // list of receivers
@@ -100,11 +99,32 @@ class productController {
       //   text: message, // plain text body
       //   html: message, // html body
       // });
+      // let mailOptions = {
+      //   from: findSeller.email, // sender address
+      //   to: email, // list of receivers
+      //   subject: `${statusBuy} Buying ${findProduct.name} ✔`, // Subject line
+      //   text: message, // plain text body
+      //   html: message, // html body
+      // };
 
+      // transporter.sendMail(mailOptions, function (error, info) {
+      //   if (error) {
+      //     console.log("/sendmail error");
+      //     console.log(error);
+      //     res.sendStatus(500);
+      //     return;
+      //   } else {
+      //     console.log("Message sent: " + info.response);
+      //     // if you don't want to use this transport object anymore, uncomment following line
+      //     socketTimeout: 30 * 1000; // 0.5 min: Time of inactivity until the connection is closed
+      //     transporter.close(); // shut down the connection pool, no more messages
+      //     res.sendStatus(200);
+      //   }
+
+      //   // if you don't want to use this transport object anymore, uncomment following line
+      //   transporter.close(); // shut down the connection pool, no more messages
+      // });
       // console.log("Message sent: %s", info.messageId);
-
-      // console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-      // console.log(info, "<<<<");
       res.status(200).json({ message: "success buy product" });
     } catch (error) {
       console.log(error);
